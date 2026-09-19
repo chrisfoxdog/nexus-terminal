51be057be9b80ac545f501c94eba5d589298564c
Chris Nobles <chrisfoxdog33@gmail.com>
2026-09-18 20:12:01 -0500

    Fix server import for Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import type { Request, Response } from "express";
-import app from "../server";
+﻿import type { Request, Response } from "express";
+import app from "../server.ts";
 
 export default function handler(req: Request, res: Response) {
   return app(req, res);
