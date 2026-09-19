2132c416d80ec605f27345847c740966cf4034f2
Chris Nobles <chrisfoxdog33@gmail.com>
2026-09-18 20:42:27 -0500

    Fix Vercel bundled API bridge

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
-﻿import type { Request, Response } from "express";
-import app from "../server.ts";
+import type { Request, Response } from 'express';
 
-export default function handler(req: Request, res: Response) {
+export default async function handler(req: Request, res: Response) {
+  const mod = await import('../dist/server.cjs');
+  const app = (mod.default as any)?.default || mod.default;
   return app(req, res);
 }
